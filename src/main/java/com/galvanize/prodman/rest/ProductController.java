ad317023ebe28eb4184e14549ba11a6186c12dd6
Kamil Grabowski <grabowski.kamil@pm.me>
2021-11-26 00:42:21 -0700

    Return user-friendly response when adding new product

--- src/main/java/com/galvanize/prodman/rest/ProductController.java
+++ src/main/java/com/galvanize/prodman/rest/ProductController.java
@@ -1,12 +1,16 @@
 package com.galvanize.prodman.rest;
 
+import com.galvanize.prodman.model.CreatedResponse;
 import com.galvanize.prodman.model.Currency;
 import com.galvanize.prodman.model.ProductDTO;
 import com.galvanize.prodman.service.ProductService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/api/", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -25,7 +29,19 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public Integer addProduct(@Valid @RequestBody ProductDTO productDTO) {
-        return productService.create(productDTO);
+    public ResponseEntity<CreatedResponse> addProduct(@Valid @RequestBody ProductDTO productDTO) {
+        final Integer productId = productService.create(productDTO);
+
+        final URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productId)
+                .toUri();
+
+        final CreatedResponse response = new CreatedResponse();
+        response.setId(productId);
+        response.setHref(uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(response);
     }
 }
